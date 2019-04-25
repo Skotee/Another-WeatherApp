0aa90afe03a86fdbb3957a3db3a5d14a8ab0eac8
Radek <radexw1@gmail.com>
2019-04-25 02:45:54 +0200

    made first template and first components

---
+++ another-weatherapp/src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//sfc //stateless function component
+
+const Form = () => {
+    return(
+        <form>
+            <input type = "text" />
+            <button>Wyszukaj miasta</button>
+        </form>
+    )
+}
+
+export default Form;
