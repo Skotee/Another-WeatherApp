5d3a9e3d8b1363f35e21246b2e774b91a3be7056
Radek <radexw1@gmail.com>
2019-04-26 04:19:40 +0200

    added fetch, and first viewing - city and temp

--- another-weatherapp/src/components/App.js
+++ another-weatherapp/src/components/App.js
@@ -3,10 +3,21 @@ import Form from "./Form";
 import Result from "./Result";
 import './App.css';
 
+//Klucz do API
+const APIKey = 'c6b14ed6cc805ac63b6b41aa3af5cfb3'
+
 class App extends Component {
 
   state = {
-      value: ""
+      value: '',
+      date: '',
+      city:'',
+      sunrise:'',
+      sunset:'',
+      temp:'',
+      pressure:'',
+      wind:'',
+      err: false, //error
     }
     handleInputChange = (e) => {
       this.setState({
@@ -14,11 +25,50 @@ class App extends Component {
       }) //zmieniamy stan zawartości inputa
     }
 
+    handleCitySubmit = (e) => {
+      e.preventDefault()
+      console.log("Potwierdzony formularz");
+      const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}`;
+
+      fetch(API) //metoda asynchroniczna, fetch tworzy obietnicę, oczekuje na rozstrzygnięcie aż dostaniemy odpowiedź - jeśli pozytywna to wykona się odpowiedź then, jeśli nie to catch
+      .then(response => {
+        if(response.ok) {
+          return response
+        }
+        throw Error("Nie udało się")
+      })
+      .then(response => response.json()) //wyodrębniamy json'a
+      .then(data => {
+        const time = new Date().toLocaleString()
+        this.setState(state => ({
+          err: false,
+          date: time,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          temp: data.main.temp,
+          pressure: data.main.pressure,
+          wind: data.wind.speed,
+          city: state.value,
+        }))
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState(state=> ({
+           err: true,
+           city: this.state.value
+          }))
+        })
+    }
+
   render() {
     return (
       <div className="App">
-        <Form value={this.state.value} change={this.handleInputChange}/>
-        <Result />
+        <Form
+          value={this.state.value}
+          change={this.handleInputChange}
+          submit={this.handleCitySubmit}
+        />
+        <Result weather={this.state}/>
       </div>
     );
   }
--- another-weatherapp/src/components/Form.js
+++ another-weatherapp/src/components/Form.js
@@ -4,8 +4,13 @@ import React from 'react';
 
 const Form = (props) => {
     return(
-        <form>
-            <input type = "text" value={props.value} onChange={props.change} placeholder="Wpisz miasto"/>
+        <form onSubmit={props.submit} >
+            <input
+                type = "text"
+                value={props.value}
+                onChange={props.change}
+                placeholder="Wpisz miasto"
+            />
             <button>Wyszukaj miasta</button>
         </form>
     )
